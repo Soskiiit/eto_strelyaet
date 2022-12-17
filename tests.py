5c056d0d1f79ecc12bf80156b2a132cbc2f8fb8f
MaxPuzotron <116840864+MaxPuzotron@users.noreply.github.com>
2022-12-17 22:30:07 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
+GPIO.setup(40,GPIO.OUT)
 servo = GPIO.PWM(11,50)
 servo.start(0)
 time.sleep(1)
@@ -13,5 +14,16 @@ while duty <= 17:
 servo.ChangeDutyCycle(2)
 time.sleep(1)
 servo.ChangeDutyCycle(0)
-Servo.stop()
+servo1 = GPIO.PWM(40,50)
+servo1.start(0)
+time.sleep(1)
+duty = 2
+while duty <= 17:
+    servo1.ChangeDutyCycle(duty)
+    time.sleep(1)
+    duty = duty + 1
+servo1.ChangeDutyCycle(2)
+time.sleep(1)
+servo1.ChangeDutyCycle(0)
+Servo1.stop()
 GPIO.cleanup()
