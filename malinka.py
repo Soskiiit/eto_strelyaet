e95673c995e30863887ca3d76673eb5798877aea
MaxPuzotron <116840864+MaxPuzotron@users.noreply.github.com>
2022-12-17 22:15:50 +0300

    Update malinka.py

--- malinka.py
+++ malinka.py
@@ -123,6 +123,21 @@ def get_cords():
         except:
             pass
 
+def povorot():
+    import RPi.GPIO as IO
+    IO.setwarnings(False)
+    IO.setmode (IO.BCM)
+    IO.setup(11,IO.OUT)
+    p = IO.PWM(19,100)
+    p.start(7.5)
+    while True:                                  
+        p.ChangeDutyCycle(7.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(1)   
+
 
 host_ip = None
 while host_ip == None:
